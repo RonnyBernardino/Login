f7e05d22d5fcc0ab3be6901cd5ddc202e77a3c61
Ronie Bernardino <roniebernardino@hotmail.com>
2022-05-06 22:02:05 -0300

    Adicionar arquivos de projeto.

---
+++ Login/Controllers/UsuariosController.cs
@@ -0,0 +1,50 @@
+﻿using Login.Entidades;
+using Login.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly Contexto db;
+
+        public UsuariosController(Contexto contexto)
+        {
+            db = contexto;
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.USUARIOS.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Usuario dadosTela)
+        {
+            if(string.IsNullOrEmpty(dadosTela.Login))
+            {
+                TempData["erro"] = "Campo Senha não pode estar em branco";
+                return View();
+            }
+
+            if (string.IsNullOrEmpty(dadosTela.Senha))
+            {
+                TempData["erro"] = "Campo Senha não pode estar em branco";
+                return View();
+            }
+
+            db.USUARIOS.Add(dadosTela);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
